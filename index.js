7956d7e933e52b9b99bcf1e2ef353c42c0112201
yuxi Shi <ys3599@tc.columbia.edu>
2024-06-10 10:38:55 -0400

    complete lab

--- index.js
+++ index.js
@@ -12,5 +12,9 @@ const tutorials = [
 ];
 
 const titleCased = () => {
-  return tutorials
-}
+  return tutorials.map(tutorial => {
+    return tutorial.split(' ').map(word => word.charAt(0).toUpperCase() + word.slice(1)).join(' ');
+  });
+};
+
+console.log(titleCased());
